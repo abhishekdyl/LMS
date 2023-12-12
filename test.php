4d81e5b6f208e97023d37762bafb5733875f779d
root <abhinegy@gmail.com>
2023-12-12 17:42:27 +0530

    git cmd

--- test.php
+++ test.php
@@ -1,3 +1,24 @@
 Welcome,
 Abhishek sir In your git. 
+ 
+-------------------Git---------------------
+
+git reset --hard HEAD~  					// reset commit log 
+git log -1  								// show last one commit log
+mkdir git_folder							// make folder
+git branch <branchname>						// create branch
+git checkout <branchname>					// switch branch
+
+
+git init 
+git status
+git branch
+git pull <url> <branch> 					// git pull https://github.com/abhishekdyl/LMS.git master
+git clone <url> 							// same as pull
+git status
+git add . 									// for add data to commit
+git commit -m "commit text write here" 		// commit cmd
+git push -u <remote_word>  <branch>      	// git push -u myorigin  moodle
+
+git remote add myorigin <url>				// to add remote keyword
 
