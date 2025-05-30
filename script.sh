72e1856ea984c5be9dda61ba67dba3e67173fd75
ABHISHEK <153192518+abhishekdyl@users.noreply.github.com>
2025-05-30 14:57:47 +0530

    ShellScript

---
+++ script.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+
+for i in {1..1000}
+do
+    echo "This is my script $i"
+done
+
+# by usin this cmd u can run this script
+# 1.    chmod +x script.sh (prepare)
+# 2.    ./script.sh (execute)
